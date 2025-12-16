ac8c5443958498c6846d464b7bdf8050b7367b99
Diogo Campas <campasdiogo@gmail.com>
2025-12-16 16:26:37 +0000

    Added inference script

---
+++ Spotify_Analysis/src/pipelines/inference_pipeline.py
@@ -0,0 +1,30 @@
+import pandas as  pd
+import joblib
+
+from src.config.settings import FEATURE_COLUMNS
+from src.steps.featurize import featurize
+
+
+class InferencePipeline:
+    def __init__(self, model_path):
+        self.model = joblib.load(model_path)
+        
+    
+    def predict(self, input_df):
+        
+        df = input_df.copy()
+        
+        missing = set(FEATURE_COLUMNS) - set(df.columns)
+        
+        if missing:
+            raise ValueError(f"Missing required features: {missing}")
+        
+        df = featurize(df)
+        
+        X = df[FEATURE_COLUMNS]
+        proba = self.model.predict_proba(X)[:,1]
+        
+        return {
+            "hit probability": float(proba[0]),
+            "prediction": int(proba[0] >= 0.5)
+        }
